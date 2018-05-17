779b62b2904eedfc920fba4a2dc22d3fdbf346d4
zongtb <zongtb@126.com>
2018-05-16 21:48:22 -0700

    fix bugs

--- main.go
+++ main.go
@@ -19,17 +19,13 @@ const (
 	//PROJECT_ID = "around-xxx"
 	//BT_INSTANCE = "around-post"
 	// Needs to update this URL if you deploy it to cloud.
-	ES_URL = "http://35.231.77.10:9200"
-
-
-
+	ES_URL = "http://35.229.51.69:9200"
 )
 
 type Location struct {
-	Lat float64 `json: "lat"`
-	Lon float64 `json: "lon"`
+	Lat float64 `json:"lat"`
+	Lon float64 `json:"lon"`
 }
-
 
 type Post struct {
 	// `json:"user"` is for the json parsing of this User field. Otherwise, by default it's 'User'.
@@ -37,7 +33,6 @@ type Post struct {
 	Message  string  `json:"message"`
 	Location Location `json:"location"`
 }
-
 
 func main() {
 	// Create a client
@@ -72,8 +67,6 @@ func main() {
 			panic(err)
 		}
 	}
-
-
 
 	fmt.Println("started server")
 	http.HandleFunc("/post", handlerPost)
@@ -123,14 +116,7 @@ func saveToES(p *Post, id string) {
 	fmt.Printf("Post is saved to Index: %s\n", p.Message)
 }
 
-
-func handlerSearch(w http.ResponseWriter, r *http.Request)  {
-	//fmt.Println("received on search request")
-	//// get lat and lon
-	//lt := r.URL.Query().Get("lat") // e.g.  "lt = "37.5""
-	//ln := r.URL.Query().Get("lon")
-
-
+func handlerSearch(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Received one request for search")
 	lat, _ := strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
 	lon, _ := strconv.ParseFloat(r.URL.Query().Get("lon"), 64)
@@ -142,33 +128,8 @@ func handlerSearch(w http.ResponseWriter, r *http.Request)  {
 	if val := r.URL.Query().Get("range"); val != "" {
 		ran = val + "km"
 	}
-	//lat,_ := strconv.ParseFloat(lt, 64) // string to float
-	//lon,_ := strconv.ParseFloat(ln, 64)
 
-	fmt.Printf( "Search received: %f %f %s\n", lat, lon, ran)
-
-	//// Return a fake post
-	//// &：取地址符
-	//p := &Post {
-	//	User:"1111",
-	//	Message:"一生必去的100个地方",
-	//	Location: Location{
-	//		Lat:lat,
-	//		Lon:lon,
-	//	},
-	//}
-	//
-	//// TO JSON object
-	//js, err := json.Marshal(p)
-	//if err != nil {
-	//	panic(err)
-	//	return
-	//}
-	//
-	//w.Header().Set("Conten-Type", "application/json")
-	//w.Write(js)
-	////fmt.Fprint(w, "Search received: ", lat, lon)
-
+	fmt.Printf("Search received: %f %f %s\n", lat, lon, ran)
 
 	// Create a client
 	client, err := elastic.NewClient(elastic.SetURL(ES_URL), elastic.SetSniff(false))
@@ -204,7 +165,8 @@ func handlerSearch(w http.ResponseWriter, r *http.Request)  {
 	// However, it ignores errors in serialization.
 	var typ Post
 	var ps []Post
-	for _, item := range searchResult.Each(reflect.TypeOf(typ)) { // instance of
+	for _, item := range searchResult.Each(reflect.TypeOf(typ)) {
+		// instance of
 		p := item.(Post) // p = (Post) item
 		fmt.Printf("Post by %s: %s at lat %v and lon %v\n", p.User, p.Message, p.Location.Lat, p.Location.Lon)
 		// TODO(student homework): Perform filtering based on keywords such as web spam etc.
@@ -220,6 +182,4 @@ func handlerSearch(w http.ResponseWriter, r *http.Request)  {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Write(js)
-
-
 }
